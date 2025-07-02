7b0c4c72cb0285fb6fc138f617a1fdd839868e9d
luna <mtk.mahsa04@gmail.com>
2025-07-02 19:10:44 +0330

    fixing errors

--- IoT_pr/Qr_code_reader.py
+++ IoT_pr/Qr_code_reader.py
@@ -3,15 +3,16 @@ from pyzbar.pyzbar import decode
 
 def read_qr(image_path):
     img = cv2.imread(image_path)
-    detected_barcodes = decode(img)
-
-    if not detected_barcodes:
-        print("QR Code wasn't found")
+    if img is None:
+        print("Could not load image")
         return False
-        
-    else:
-        for barcode in detected_barcodes:
-            data = barcode.data.decode('utf-8')
-            print("QR Code data:", data)
-            return data
 
+    barcodes = decode(img)
+    if not barcodes:
+        print("No QR code detected")
+        return False
+
+    for barcode in barcodes:
+        data = barcode.data.decode('utf-8')
+        print("Detected QR:", data)
+        return data
